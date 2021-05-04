651673d259f82144048b893c0644b3529bdb6a22
Aldemar <ramirez_aldemar@javeriana.edu.co>
2021-05-04 17:37:47 -0500

    Event2

--- barberia/src/main/java/com/proyectoweb/barberia/Services/Schedule/Domain/ValueObjects/ScheduleDate.java
+++ barberia/src/main/java/com/proyectoweb/barberia/Services/Schedule/Domain/ValueObjects/ScheduleDate.java
@@ -1,5 +1,24 @@
 package com.proyectoweb.barberia.Services.Schedule.Domain.ValueObjects;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class ScheduleDate {
+
+    // ValueObject para el Date?
+    private GregorianCalendar value;
+
+    public ScheduleDate(GregorianCalendar value) {
+        this.value = value;
+    }
+
+    public GregorianCalendar value() {
+        return value;
+    }
+
+    private void validate(String value) {
+        //TODO: Other Rules Service name
+    }
+
 
 }
--- barberia/src/main/java/com/proyectoweb/barberia/Shared/Domain/Services/ScheduleCreatedDomainEvent.java
+++ barberia/src/main/java/com/proyectoweb/barberia/Shared/Domain/Services/ScheduleCreatedDomainEvent.java
@@ -3,25 +3,73 @@ package com.proyectoweb.barberia.Shared.Domain.Services;
 import com.proyectoweb.barberia.Shared.Domain.Bus.Event.DomainEvent;
 
 import java.io.Serializable;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class ScheduleCreatedDomainEvent extends DomainEvent {
 
-    //private final String scheduleId;
-    //private final
+    private final String scheduleId;
+    private final GregorianCalendar scheduleDateStart;
+    private final GregorianCalendar scheduleDateEnd;
+
+    public ScheduleCreatedDomainEvent() {
+        super(null);
+        this.scheduleId = "";
+        this.scheduleDateStart = new GregorianCalendar();
+        this.scheduleDateEnd = new GregorianCalendar();
+    }
+
+    public ScheduleCreatedDomainEvent(String aggregateId, String eventId, String occurredOn, String scheduleId, GregorianCalendar scheduleDateStart, GregorianCalendar scheduleDateEnd) {
+        super(aggregateId, eventId, occurredOn);
+        this.scheduleId = scheduleId;
+        this.scheduleDateStart = scheduleDateStart;
+        this.scheduleDateEnd = scheduleDateEnd;
+    }
+
+    public ScheduleCreatedDomainEvent(String scheduleId, GregorianCalendar scheduleDateStart, GregorianCalendar scheduleDateEnd) {
+        this.scheduleId = scheduleId;
+        this.scheduleDateStart = scheduleDateStart;
+        this.scheduleDateEnd = scheduleDateEnd;
+    }
+
 
     @Override
     public String eventName() {
-        return null;
+        return "schedule.created";
     }
 
     @Override
     public HashMap<String, Serializable> toPrimitive() {
-        return null;
+        return new HashMap<String, Serializable>() {{
+            put("scheduleId", scheduleId);
+            put("scheduleDateStart", scheduleDateStart);
+            put("scheduleDateEnd", scheduleDateEnd);
+        }};
     }
 
     @Override
     public DomainEvent fromPrimitive(String aggregateId, HashMap<String, Serializable> body, String eventId, String occurredOn) {
-        return null;
+        return new ScheduleCreatedDomainEvent(
+                aggregateId,
+                eventId,
+                occurredOn,
+                (String) body.get("scheduleId"),
+                (GregorianCalendar) body.get("scheduleDateStart"),
+                (GregorianCalendar) body.get("scheduleDateEnd")
+        );
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ScheduleCreatedDomainEvent that = (ScheduleCreatedDomainEvent) o;
+        return Objects.equals(scheduleId, that.scheduleId) && Objects.equals(scheduleDateStart, that.scheduleDateStart) && Objects.equals(scheduleDateEnd, that.scheduleDateEnd);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(scheduleId, scheduleDateStart, scheduleDateEnd);
     }
 }
